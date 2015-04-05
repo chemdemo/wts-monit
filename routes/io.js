755585139ed86f90d007304bd19b659aad6316fc
chemdemo <yangdemo@gmail.com>
2015-04-05 23:39:55 +0800

    rename

---
+++ routes/io.js
@@ -0,0 +1,67 @@
+/*
+* @Author: dm.yang
+* @Date:   2015-04-03 18:29:55
+* @Last Modified by:   dm.yang
+* @Last Modified time: 2015-04-05 23:14:34
+*/
+
+'use strict';
+
+var _ = require('lodash');
+
+var clientsMgr = require('../lib/clients_manager');
+var logger = require('../helpers/logger').socket;
+
+exports.route = function(io) {
+    io.on('connection', onConn);
+};
+
+function onConn(sock) {
+    sock.on('term:conf', onSockConf.bind({socket: sock}));
+    sock.on('term:input', onSockInput.bind({socket: sock}));
+    sock.on('disconnect', onSockDisconnect.bind({socket: sock}));
+    sock.on('error', onSockError.bind({socket: sock}));
+};
+
+// create session between term and remote client
+// maybe use socket.io session is better
+function onSockConf(clientId, termId) {
+    var socket = this.socket;
+    var client = clientsMgr.getClientById(clientId);
+
+    socket.clientId = clientId;
+    socket.termId = termId;
+    if(client) client.addTerm(termId, socket);
+};
+
+function onSockInput(cmd) {
+    var socket = this.socket;
+    var cid = socket.clientId;
+    var termId = socket.termId;
+    var client = clientsMgr.getClientById(cid);
+
+    if(!client) {
+        var output = 'Client Not Found.'
+        socket.emit('client:output', output);
+        logger.warn(output);
+        return;
+    }
+
+    client.sendMsg({cmd: 'term:input', termId: termId, input: cmd});
+};
+
+function onSockError(err) {
+    logger.error(err.stack);
+};
+
+function onSockDisconnect() {
+    var socket = this.socket;
+    var client;
+
+    logger.info('socket disconnect');
+
+    if(socket) {
+        client = clientsMgr.getClientById(socket.clientId);
+        if(client) client.removeTerm(socket.termId);
+    }
+};
