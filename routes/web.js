e13b2970a176ec8cc196567d780248247ca1c093
chemdemo <yangdemo@gmail.com>
2015-04-08 12:20:00 +0800

    add gzip middleware

--- routes/web.js
+++ routes/web.js
@@ -1,14 +1,15 @@
 /*
 * @Author: dm.yang
 * @Date:   2015-04-03 16:29:45
-* @Last Modified by:   chemdemo
-* @Last Modified time: 2015-04-08 00:32:00
+* @Last Modified by:   dm.yang
+* @Last Modified time: 2015-04-08 12:18:55
 */
 
 'use strict';
 
 var fs = require('fs');
 var path = require('path');
+var gzip = require('koa-gzip');
 var _ = require('lodash');
 
 var clientsMgr = require('../lib/clients_manager');
@@ -19,7 +20,7 @@ exports.route = function(app) {
         yield this.render('index', {group: clientsMgr.getGroup()});
     });
 
-    app.get('/tmpl/list', function* () {
+    app.get('/tmpl/list', gzip(), function* () {
         this.body = fs.createReadStream(clientTmplPath);
     });
 
@@ -31,11 +32,11 @@ exports.route = function(app) {
         yield this.render('term', {host: client.host, port: client.port, clientId: client.id, termId: tid});
     });
 
-    // app.post('/client/destroy/:clientId', function* () {
-    //     var cid = this.params['clientId'];
+    /*app.post('/client/destroy/:clientId', function* () {
+        var cid = this.params['clientId'];
 
-    //     if(clientsMgr.removeClient(cid)) this.body = {code: 0};
-    //     else this.body = {code: 1};
-    // });
+        if(clientsMgr.removeClient(cid)) this.body = {code: 0};
+        else this.body = {code: 1};
+    });*/
 };
 
